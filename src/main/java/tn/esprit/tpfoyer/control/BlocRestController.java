ad07433a1d10f7c6763497139faedcb2bf54c108
oussema269 <80462158+oussema269@users.noreply.github.com>
2024-11-02 15:58:41 +0100

    angular

--- src/main/java/tn/esprit/tpfoyer/control/BlocRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/BlocRestController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/bloc")
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BlocRestController {
 
     IBlocService blocService;
